936700c1b839b5445ff42809e5e1df97662d736e
merrithenr <henrymrrtt1@gmail.com>
2019-03-10 22:24:37 +1300

    starting of the project

--- mainF.py
+++ mainF.py
@@ -4,20 +4,23 @@ Created on Fri Mar  8 12:12:23 2019
 
 @author: Henry
 """
+#importing a language package in order to tokenize the words
 import nltk
-nltk.download('punkt')
 
-main()
+#Defining the main and letting it be able to be called and must go through this every time
 def main():
+    # downloads data, reads the data and places this sentence into a variable, then placing it into the tokenizer
     data = load_data()
-    print("okay here")
-    data.read()
-    print(data)
+    data = data.read()
     tokens = tokenize(data)
 
+#opens the file and downloads the file
 def load_data():
     return open("test.txt","r")
 
+#tokenizes the sentece with word tokenizer and returns these individual tokens in an array.
 def tokenize(data):
-    tokens = nltk.tokenizer.word_tokenize(data)
+    tokens = nltk.word_tokenize(data)
     return tokens
+
+main()
---
+++ train.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 22:07:09 2019
+
+@author: Henry
+"""
+
